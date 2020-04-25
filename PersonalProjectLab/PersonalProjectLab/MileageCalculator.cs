73cc0d34cffd412e41be67c54c74a7be07e1a375
ASchmit2 <aschmit2019@gmail.com>
2020-04-25 17:32:05 -0500

    Create MileageCalculator.cs

---
+++ PersonalProjectLab/PersonalProjectLab/MileageCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace PersonalProjectLab
+{
+    public class MileageCalculator
+    {
+
+        public MileageCalculator()
+        {
+        }
+
+        public double MilesToEmpty(double mileage, double tankSize, double fuelLevel)
+        {
+            double output = 0;
+
+            output = tankSize * fuelLevel * mileage;
+            
+            return output;
+        }
+
+        public double MilesToEmpty2(double milesPerTank, double fuelLevel)
+        {
+            double output = 0;
+
+            output = milesPerTank * fuelLevel;
+
+            return output;
+        }
+
+        public double CalculateMileage(double milesPerTank, double tankSize)
+        {
+            double output = 0;
+
+            output = milesPerTank / tankSize;
+
+            return output;
+        }
+    }   
+}
